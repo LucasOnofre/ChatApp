7a684201f32518d10e6e241e186b49d9461a97f9
Lucas <lucas-onofre@hotmail.com>
2018-03-02 17:17:48 -0300

    Initial commit

---
+++ app/src/main/java/lucas/br/whatsapp/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package lucas.br.whatsapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import lucas.br.whatsapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/lucas/br/whatsapp/activity/ValidadorActivity.java
@@ -0,0 +1,15 @@
+package lucas.br.whatsapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lucas.br.whatsapp.R;
+
+public class ValidadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validador);
+    }
+}
